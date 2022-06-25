9624fdccfe39281bccb5835df2b1122c6526a635
AdisaQudrah <qudrahoyin@gmail.com>
2022-06-25 15:30:16 +0100

    no changes

---
+++ script.js
@@ -0,0 +1,9 @@
+var firstname = 'ADISA QUDRAH'
+var height = '5ft 8'
+var country = 'NIGERIA'
+
+console.log(firstname)
+console.log(height)
+console.log(country)
+
+document.write(firstname + "<br>" + height +"<br>" + country)
